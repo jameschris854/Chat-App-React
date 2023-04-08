b7270ee611be29265827074c6147ad5e9334cafd
jameschris854 <jameschris854@gmail.com>
2023-04-08 12:03:35 +0530

    [ENHANCEMENT] - service workers added and pwa supported

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import * as ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
-import theme from "./theme";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
 import { useMediaQuery } from "@mui/material";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
@@ -41,3 +41,10 @@ const Root = () => {
 }
 
 root.render(<Root />);
+console.log('LETS REGISTER')
+serviceWorkerRegistration.register({onSuccess(registration) {
+  console.log('serviceWorkerRegistration onSuccess',registration)
+},
+onUpdate(registration) {
+  console.log('serviceWorkerRegistration onUpdate',registration)
+},});
